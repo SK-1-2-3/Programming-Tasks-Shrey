03f56f1c1d431eab57f84c2e5166c7c6ce85f718
Shrey <45907@rws.uk.net>
2026-09-10 17:45:57 +0100

    done it

--- Week_01_Core_Fundamentals/06_temperature_converter.py
+++ Week_01_Core_Fundamentals/06_temperature_converter.py
@@ -13,9 +13,21 @@ TODO:
 """
 
 def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
+    celsius = input("enter temperature you want to convert, type stop when finished:")
+    while celsius != "stop":
+      if celsius.isdigit():
+       celsius = int(celsius)
+       fahrenheit = (celsius * 1.8) + 32
+       print("your temperature in fahrenheit is", fahrenheit)
+      else:
+       print("error enter a number")
+
+      celsius = input("enter temperature you want to convert, type stop when finished:")
+
+    print("Bye")
+
+
+
     pass
 
 
