420f0d124e470e56b637f13485ea82bebb508953
Shrey.k <45907@rws.uk.net>
2026-09-19 12:11:02 +0100

    done

--- Week_02_GCSE_Basics/04_Password_Strength.py
+++ Week_02_GCSE_Basics/04_Password_Strength.py
@@ -15,9 +15,41 @@ TODO:
 """
 
 def main():
-    # TODO: Write demonstration/testing code
-    # If you want to delete all the code here and work just with a blank file go ahead, remember anything under the if __name__=="__main__":
-    # will only run if this module is being run directly. So used this subprocedure to carry out testing if it is going to be an imported file.
+password = input("enter password :")
+lengths = len(password)
+has_num = False
+has_capital_lower = False
+special = False
+length = False
+has_capital = False
+has_lower = False
+for character in password:
+        if character.isdigit():
+            has_num = True
+        elif character.isupper():
+            has_capital = True
+        elif character.islower():
+            has_lower = True
+        elif not character.isalnum():
+            special = True
+if lengths >= 8:
+    length = True
+if has_capital == True and has_lower == True:
+    has_capital_lower = True
+
+total = sum([has_num, has_capital_lower, special, length])
+if total == 0:
+    print("You have weak password")
+elif total == 1:
+    print("You have a weak password!")
+elif total == 2:
+    print("You have a medium password!")
+elif total == 3:
+    print("You have a medium password!")
+elif total == 4:
+    print("You have a strong password!")
+
+
     pass
 
 
